9a31d44e304367f375fdff89fdf68056f63fc245
santro93 <nsantosh435@gmail.com>
2022-03-20 17:18:50 +0530

    Logc for equality of two lines

--- OOPS_Line_Comparison.java
+++ OOPS_Line_Comparison.java
@@ -3,20 +3,24 @@ package com.bridgelabz.oopslinecomparison;
 import java.util.Scanner;
 
 public class OOPS_Line_Comparison {
-
+	//declared global variables
+	static Double length1;
+    static Double length2;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Problem!");
 		
 		//method call
 		OOPS_Line_Comparison lineComparison = new OOPS_Line_Comparison();
 		lineComparison.lengthCalculation();
-		
+		lineComparison.lengthComparison();
 	}
 	
 	 //method: length calculation	
 	 public static void lengthCalculation(){
 
 			double x1,x2,y1,y2;
+			double x3,x4,y3,y4;
 			Scanner sc = new Scanner(System.in);
 
 			System.out.println("Enter the  Co-ordinates of 1st Line");
@@ -24,9 +28,29 @@ public class OOPS_Line_Comparison {
 			x2 = sc.nextDouble();
 			y1 = sc.nextDouble();
 			y2 = sc.nextDouble();
-				sc.close();
 				
-			double length = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 -y1),2));
-			System.out.println("Length of Line = "+length);
-		}
+				
+			double length1 = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 -y1),2));
+			System.out.println("Length of 1st Line: "+length1);
+			
+			System.out.println("Enter the  Co-ordinates of 2nd Line");
+            x3 = sc.nextDouble();
+            x4 = sc.nextDouble();
+            y3 = sc.nextDouble();
+            y4 = sc.nextDouble();
+              sc.close();
+              
+            length2 = Math.sqrt(Math.pow((x4 - x3),2) + Math.pow((y4 -y3),2));
+            System.out.println("Length of 2nd Line: "+length2);	    
+         }
+	 
+	 public static void lengthComparison(){
+         if(length1 == length2){
+                 System.out.println("The length of line are equals");
+         }
+         else
+         {
+               System.out.println("The length of line are not equals");
+         }
+   }
 }
